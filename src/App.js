a513f4bbb69e4ad7d5ccb0d577d6ffb081447e29
danprasetyoo <danprasetyoo21@gmail.com>
2022-08-05 00:03:19 +0700

    day 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
 function App() {
+
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <CartProvider>
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
-}
+};
 
 export default App;
